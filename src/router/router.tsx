64d0ec1b881419814c77a1d43df8e26ded13cb46
Andrei Klimkou <aaklimkov@gmail.com>
2025-05-07 16:41:31 +0300

    feat: change vrowser to hash router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { createBrowserRouter } from 'react-router-dom'
+import { createHashRouter } from 'react-router-dom'
 import PlayerPage from '../pages/PlayerPage'
 import PlayersTablePage from '../pages/PlayersTablePage'
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
 	{
 		path: '/',
 		element: <PlayersTablePage />,
@@ -12,4 +12,4 @@ export const router = createBrowserRouter([
 		path: 'player/:id',
 		element: <PlayerPage />,
 	},
-], { basename: '/statistic' })
+])
