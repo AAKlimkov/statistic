9bbba0b8b727b25f6de6f89e677682f4533f3825
Andrei Klimkou <aaklimkov@gmail.com>
2025-06-29 20:37:18 +0300

    feat: add result U-1/8-4

--- src/modules/Fantasy/data/result.ts
+++ src/modules/Fantasy/data/result.ts
@@ -12,8 +12,13 @@ export const tournamentResults: Record<string, MatchResult> = {
 		losers: [15, 18, 29, 55],
 	},
 	'U-1/8-3': {
-		winners: [132, 34, 125, 110, 72	],
+		winners: [132, 34, 125, 110, 72],
 		places: { 6: [225] },
 		losers: [69, 90, 63, 109],
+	},
+	'U-1/8-4': {
+		winners: [116, 47, 7, 71, 77],
+		places: { 6: [43] },
+		losers: [54, 94, 95, 231],
 	},
 }
